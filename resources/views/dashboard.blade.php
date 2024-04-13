98c71de55b62a9b902c617fb4eebd85cf64c7828
MeleanDev <insightacosta@gmail.com>
2024-04-13 09:03:49 -0400

    config basic

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,59 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('adminlte.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+{{-- Customize layout sections --}}
+
+@section('subtitle', 'Panel Principal')
+@section('content_header_title', 'Inicio')
+@section('content_header_subtitle', 'Panel Principal')
+
+{{-- Content body: main page content --}}
+
+@section('content_body')
+    <div class="container">
+        <div class="col-lg-8 mb-4 order-0 mx-auto">
+            <div class="card">
+                <div class="d-flex align-items-end row">
+                    <div class="col-sm-7">
+                        <div class="card-body">
+                            <h5 class="card-title text-primary">Bienvenido {{auth()->user()->name}}!! 🎉</h5>
+                            <p class="mb-2">
+                            Al sistema de administracion de tu comercio. <br><br>Sistema desarrollado por MeleanDev.
+                            </p>
+                    
+                            <a href="https://github.com/MeleanDev" class="btn btn-sm btn-outline-primary">Contactar con el Desarrollador</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-5 text-center text-sm-left">
+                        <div class="card-body pb-0 px-0 px-md-4">
+                            <img
+                            src="{{ asset("img/panelcard.png") }}"
+                            height="140"
+                            alt="img del sistema"
+                            data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                            data-app-light-img="illustrations/man-with-laptop-light.png"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+@stop
+
+{{-- Push extra CSS --}}
+
+@push('css')
+
+    {{-- Add here extra stylesheets --}}
+
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+
+@endpush
+
+{{-- Push extra scripts --}}
+
+@push('js')
+    <script>
+        
+    </script>
+@endpush
