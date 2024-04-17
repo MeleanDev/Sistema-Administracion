651a8f50c9377d1aa071d1347a7c1c2042a01038
MeleanDev <insightacosta@gmail.com>
2024-04-17 09:51:01 -0400

    registro

--- app/Http/Controllers/RegistroActividadesController.php
+++ app/Http/Controllers/RegistroActividadesController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\RegistroActividad;
+use App\RegistroActividadesClass;
 use Illuminate\Http\Request;
 
 class RegistroActividadesController extends Controller
 {
+    private $registro;
+
+    public function __construct(RegistroActividadesClass $registro)
+    {
+        $this->registro = $registro;
+    }
+
     public function index(Request $request){
         if ($request->ajax()) {
              $datos = RegistroActividad::all();
@@ -15,12 +23,13 @@ class RegistroActividadesController extends Controller
          return view('panelAdmin.RegistroActividades');
      }
 
-    public function eliminar(Request $request){
-        $data = $request->all();
-        $seguro = $data['seguro'];
+    public function eliminar(Request $data){
 
+        $seguro = $data['seguro'];
         if ($seguro == 1) {
             RegistroActividad::truncate();
+            $this->registro->ActividadRegistro("Todos","Eliminar","Registro");
+
             $respuesta = response()->json(['success' => true]);
         }
         return $respuesta;
